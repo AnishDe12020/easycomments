2648ce43bb9f4fb28e16f92abd802f43a5271bec
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-08-20 14:16:33 +0530

    Added authentication with auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css"
 import { ChakraProvider } from "@chakra-ui/react"
 import theme from "@/styles/theme"
+import { UserProvider } from "@auth0/nextjs-auth0"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
     </ChakraProvider>
   )
 }
