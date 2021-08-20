71a40f765ac935d0c50282fe913f62d7de1febab
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-08-20 12:34:40 +0530

    Added chakra UI and did the initial set up for the same. Also removed some unused stylws

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Easy Comments</title>
         <meta
