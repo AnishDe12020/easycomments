95a095c841c7b97e5d82abfe4831bcff97179021
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-08-23 13:46:44 +0530

    some styling changes to the comment component and showed the timestamp

--- components/Comment.js
+++ components/Comment.js
@@ -1,11 +1,18 @@
-import { Box, Heading, Text } from "@chakra-ui/react"
+import { Box, Heading, Text, Flex } from "@chakra-ui/react"
 import React from "react"
 
 const Comment = ({ authorName, comment, createdAt }) => {
   return (
     <Box m={8} p={4}>
-      <Heading>{authorName}</Heading>
-      <Text size="lg">{comment}</Text>
+      <Flex align="end" p={0.5}>
+        <Heading fontSize="2xl">{authorName}</Heading>
+        <Text fontSize="md" ml={2}>
+          {createdAt}
+        </Text>
+      </Flex>
+      <Text fontSize="lg" p={0.5}>
+        {comment}
+      </Text>
     </Box>
   )
 }
