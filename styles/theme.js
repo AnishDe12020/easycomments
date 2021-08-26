56bf8b9d48c04e242680f6dfc8cedd6c667c2475
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-08-26 17:42:09 +0530

    created some breakpoints for responsive styles

--- styles/theme.js
+++ styles/theme.js
@@ -1,10 +1,18 @@
 import { extendTheme } from "@chakra-ui/react"
+import { createBreakpoints } from "@chakra-ui/theme-tools"
 
 const config = {
   initialColorMode: "dark",
   useSystemColorMode: true,
 }
 
-const theme = extendTheme({ config })
+const breakpoints = createBreakpoints({
+  sm: "320px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+})
+
+const theme = extendTheme({ config, breakpoints })
 
 export default theme
