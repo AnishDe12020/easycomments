5136d09ce1202c93460cba136f057419e62c4ed9
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-08-27 16:08:24 +0530

    added a page load progress bar with next-progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import "../styles/globals.css"
 import { ChakraProvider } from "@chakra-ui/react"
 import theme from "@/styles/theme"
 import { UserProvider } from "@auth0/nextjs-auth0"
+import NextNProgress from "nextjs-progressbar"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <UserProvider>
+        <NextNProgress />
         <Component {...pageProps} />
       </UserProvider>
     </ChakraProvider>
