64da81a6ec89a16e1a73058b1904f9d7358b1fc6
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-08-27 20:22:39 +0530

    THe copy button now copies the value to the clipboard

--- components/CoppyEmbedLinkModal.js
+++ components/CoppyEmbedLinkModal.js
@@ -1,4 +1,4 @@
-import { CopyIcon } from "@chakra-ui/icons"
+import { CheckIcon, CopyIcon } from "@chakra-ui/icons"
 import {
   Modal,
   ModalOverlay,
@@ -13,13 +13,25 @@ import {
   Flex,
   IconButton,
   useDisclosure,
+  useClipboard,
 } from "@chakra-ui/react"
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const CopyEmbedLinkModal = ({ site }) => {
   const [route, setRoute] = useState("/")
   const [preferredColorMode, setPreferredColorMode] = useState()
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const [url, setUrl] = useState("")
+
+  useEffect(() => {
+    setUrl(
+      `https://easycomments.anishde.dev/embed/${site.id}${route}${
+        preferredColorMode ? "?colorMode=" + preferredColorMode : ""
+      }`
+    )
+  }, [route, preferredColorMode])
+
+  const { hasCopied, onCopy } = useClipboard(url)
 
   const handleRouteChanged = e => {
     setRoute(e.target.value)
@@ -47,15 +59,13 @@ const CopyEmbedLinkModal = ({ site }) => {
             </Select>
 
             <Flex mt={4}>
-              <Input
-                value={`https://easycomments.anishde.dev/embed/${
-                  site.id
-                }${route}${
-                  preferredColorMode ? "?colorMode=" + preferredColorMode : ""
-                }`}
-                readOnly
+              <Input value={url} isReadOnly />
+              <IconButton
+                onClick={onCopy}
+                ml={2}
+                icon={hasCopied ? <CheckIcon /> : <CopyIcon />}
+                backgroundColor={hasCopied ? "green.500" : ""}
               />
-              <IconButton ml={2} icon={<CopyIcon />} />
             </Flex>
           </ModalBody>
 
