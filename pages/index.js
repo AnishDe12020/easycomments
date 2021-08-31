c4eed73eab3d8565afd37437e207babf196cf406
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-08-31 18:22:07 +0530

    small styling change on the home page

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
         </Center>
         <Features mt={8} />
       </Flex>
-      <Box m={{ sm: 2, md: 4, lg: 8 }} height="100%">
+      <Box m={{ sm: 2, md: 4, lg: 8 }}>
         <IframeResizer
           style={{
             width: "1px",
